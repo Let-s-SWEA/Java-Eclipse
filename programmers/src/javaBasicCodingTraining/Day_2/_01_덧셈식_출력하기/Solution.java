c2a542dc769e16f68e33c60d13c7dfc13602e0f2
HUN <dawin00k@gmail.com>
2026-03-11 17:29:19 +0900

    Day2 : Solved Printing Addition Formual

--- programmers/src/javaBasicCodingTraining/Day_2/_01_덧셈식_출력하기/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_2/_01_덧셈식_출력하기/Solution.java
@@ -1,3 +1,15 @@
-//
+//4 5
 
 package javaBasicCodingTraining.Day_2._01_덧셈식_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+}
