05c65ce89ae74c5c89687dfb1f3cbfd2864e8674
HUN <dawin00k@gmail.com>
2026-03-12 18:36:30 +0900

    day3: Convert a char array to a string

---
+++ programmers/src/javaBasicCodingTraining/Day_3/_02_문자_리스트를_문자열로_변환하기/Solution.java
@@ -0,0 +1,30 @@
+// input arr : ["a","b","c"]
+// output result : "abc"
+
+package javaBasicCodingTraining.Day_3._02_문자_리스트를_문자열로_변환하기;
+
+class Solution {
+	public String solution(String[] arr) {
+		StringBuilder sb = new StringBuilder();
+
+		for (String i : arr) {
+			sb.append(i);
+		}
+		
+		String answer = sb.toString();
+
+		return answer;
+	}
+	
+	public String solution2(String[] arr) {
+		return String.join("",  arr);
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+		String result = sol.solution(new String[] {"a", "b", "c"});
+		
+		System.out.println(result);
+	}
+}
