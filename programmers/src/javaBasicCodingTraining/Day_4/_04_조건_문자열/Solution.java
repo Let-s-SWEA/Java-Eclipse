60e3b435bb15bfa0ffbc77658ad89ecb9fc19a55
HUN <dawin00k@gmail.com>
2026-03-17 22:31:45 +0900

    day4: Condition String

--- programmers/src/javaBasicCodingTraining/Day_4/_04_조건_문자열/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_4/_04_조건_문자열/Solution.java
@@ -1,18 +1,32 @@
-// input : 
-// output :
+// ineq : "<"
+// eq : "="
+// n : 20
+// m : 50
+// result : 1
 
 package javaBasicCodingTraining.Day_4._04_조건_문자열;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int solution(String ineq, String eq, int n, int m) {
+		switch (ineq + eq) {
+		case "<=":
+			return n <= m ? 1 : 0;
+		case ">=":
+			return n >= m ? 1 : 0;
+		case "<!":
+			return n < m ? 1 : 0;
+		case ">!":
+			return n > m ? 1 : 0;
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		int result = sol.solution(">", "=", 41, 78);
+
 		System.out.println(result);
 	}
 }
