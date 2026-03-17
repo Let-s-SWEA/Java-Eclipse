ba289c69bb7d8f3a5f9b1f2d7f0fecdfef5fe33d
HUN <dawin00k@gmail.com>
2026-03-17 18:49:16 +0900

    day3: Comparing the opertaion values of two numbers

--- programmers/src/javaBasicCodingTraining/Day_3/_05_두_수의_연산값_비교하기/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_3/_05_두_수의_연산값_비교하기/Solution.java
@@ -1,11 +1,19 @@
-// input : 
-// output : 
+// input a : 2
+// input b : 91
+// output result : 364 
 
 package javaBasicCodingTraining.Day_3._05_두_수의_연산값_비교하기;
 
 class Solution{
-	public String solution() {
-		String answer = "";
-		return answer;
+	public int solution(int a, int b) {
+		return Math.max(Integer.parseInt(String.valueOf(a) + "" + b), 2 * a * b);
+	}
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		int result = sol.solution(91, 2);
+		
+		System.out.println(result);
 	}
 }
