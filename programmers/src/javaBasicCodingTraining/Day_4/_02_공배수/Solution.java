ed6b06ce9f87baab50a32db486e33c10278b91b9
HUN <dawin00k@gmail.com>
2026-03-17 20:05:42 +0900

    day4: Return different values depending on odd or even

--- programmers/src/javaBasicCodingTraining/Day_4/_02_공배수/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_4/_02_공배수/Solution.java
@@ -1,18 +1,25 @@
-// input : 
-// output :
+// input number : 60
+// input n : 2
+// input m : 3
+// output result : 1
+
+// input number : 55
+// input n : 10
+// input m : 5
+// output result : 0
 
 package javaBasicCodingTraining.Day_4._02_공배수;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int solution(int number, int n, int m) {
+		return number % n == 0 && number % m == 0 ? 1 : 0;
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		int result = sol.solution(55, 10, 5);
+
 		System.out.println(result);
 	}
 }
