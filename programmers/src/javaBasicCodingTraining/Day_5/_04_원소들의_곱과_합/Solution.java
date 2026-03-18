eb18be4bb59df5bdc8048ee569073a5a336f70c6
HUN <dawin00k@gmail.com>
2026-03-18 23:11:04 +0900

    day5: Concatenated Numbers

--- programmers/src/javaBasicCodingTraining/Day_5/_04_원소들의_곱과_합/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_5/_04_원소들의_곱과_합/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	public int solution(int[] num_list) {
 		int addSum = num_list[0];
 		int mulSum = num_list[0];
-
+		
 		for (int i = 1; i < num_list.length; i++) {
 			addSum += num_list[i];
 			mulSum *= num_list[i];
@@ -26,17 +26,15 @@ public class Solution {
 			mulSum *= num;
 		}
 		
-		System.out.println(addSum + " " + mulSum);
-		
 		if(mulSum < addSum * addSum) return 1;
 		else return 0;
 	}
-
+	
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-
+		
 		int result = sol.solution2(new int[] { 3, 4, 5, 2, 1 });
-
+		
 		System.out.println(result);
 	}
 }
