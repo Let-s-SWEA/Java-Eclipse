d7084c17e837484e2652ded4794bfdcb1f8e2430
HUN <dawin00k@gmail.com>
2026-03-18 16:21:59 +0900

    day1: Subtraction, Multiply, dividing

--- programmers/src/javaBignner/Day_1/_02_두_수의_차_구하기/Solution.java
+++ programmers/src/javaBignner/Day_1/_02_두_수의_차_구하기/Solution.java
@@ -1,18 +1,23 @@
-// 
-// 
+// num1 : 2
+// num2 : 3
+// result : -1
+
+// num1 : 100
+// num2 : 2
+// result : 98
 
 package javaBignner.Day_1._02_두_수의_차_구하기;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int solution(int num1, int num2) {
+		return num1 - num2;
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		int result = sol.solution(2, 3);
+
 		System.out.println(result);
 	}
 }
--- programmers/src/javaBignner/Day_1/_03_두_수의_곱_구하기/Solution.java
+++ programmers/src/javaBignner/Day_1/_03_두_수의_곱_구하기/Solution.java
@@ -1,18 +1,23 @@
-// 
-// 
+// num1 : 3
+// num2 : 4
+// result : 12
+
+// num1 : 27
+// num2 : 19
+// result : 513
 
 package javaBignner.Day_1._03_두_수의_곱_구하기;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int solution(int num1, int num2) {
+		return num1 * num2;
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		int result = sol.solution(3, 4);
+
 		System.out.println(result);
 	}
 }
