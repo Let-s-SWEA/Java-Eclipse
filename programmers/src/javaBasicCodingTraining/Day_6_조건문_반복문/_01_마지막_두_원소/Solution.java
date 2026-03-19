c0fd78c1b1f4bc7fb05f14901df34804a17b5bc8
HUN <dawin00k@gmail.com>
2026-03-19 18:54:42 +0900

    day6: Last Two Elements

--- programmers/src/javaBasicCodingTraining/Day_6_조건문_반복문/_01_마지막_두_원소/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_6_조건문_반복문/_01_마지막_두_원소/Solution.java
@@ -1,19 +1,26 @@
-//
-//
-//
-//
+// num_list : [2, 1, 6]
+// result : [2, 1, 6, 5]
 
 package javaBasicCodingTraining.Day_6_조건문_반복문._01_마지막_두_원소;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int[] solution(int[] num_list) {
+		int last = num_list[num_list.length - 1];
+		int prev = num_list[num_list.length - 2];
+		int[] answer = new int[num_list.length + 1];
+
+		for (int i = 0; i < num_list.length; i++) {
+			answer[i] = num_list[i];
+		}
+		answer[answer.length - 1] = last > prev ? last - prev : last * 2;
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		int[] result = sol.solution(new int[] { 2, 1, 6 });
 
 		System.out.println(result);
 	}
