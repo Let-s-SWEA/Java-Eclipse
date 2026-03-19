bb3ea6df046b1ddd130b5600d6c4974b72f3f3b1
HUN <dawin00k@gmail.com>
2026-03-20 07:57:51 +0900

    day7: Setting

---
+++ programmers/src/javaBasicCodingTraining/Day_7_반복문/_02_배열_만들기_2/Solution.java
@@ -0,0 +1,18 @@
+//
+//
+
+package javaBasicCodingTraining.Day_7_반복문._02_배열_만들기_2;
+
+public class Solution {
+	public int solution(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+		int result = sol.solution(0, 0);
+
+		System.out.println(result);
+	}
+}
---
+++ programmers/src/javaBasicCodingTraining/Day_7_반복문/_04_콜라츠_수열_만들기/Solution.java
@@ -0,0 +1,18 @@
+//
+//
+
+package javaBasicCodingTraining.Day_7_반복문._04_콜라츠_수열_만들기;
+
+public class Solution {
+	public int solution(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+		int result = sol.solution(0, 0);
+
+		System.out.println(result);
+	}
+}
