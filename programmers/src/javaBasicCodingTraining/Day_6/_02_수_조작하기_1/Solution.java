8562511ed1b1bdae3b3e8d4cba25b858ddf0e536
HUN <dawin00k@gmail.com>
2026-03-19 18:28:30 +0900

    day6: Setting

---
+++ programmers/src/javaBasicCodingTraining/Day_6/_02_수_조작하기_1/Solution.java
@@ -0,0 +1,20 @@
+//
+//
+//
+//
+
+package javaBasicCodingTraining.Day_6._02_수_조작하기_1;
+
+public class Solution {
+	public int solution(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+		int result = sol.solution(0, 0);
+
+		System.out.println(result);
+	}
+}
