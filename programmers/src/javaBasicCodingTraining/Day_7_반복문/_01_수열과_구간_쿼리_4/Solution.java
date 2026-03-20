8b35febe68a4731c72fd1ee6bf4a5d2ab3c2d8aa
HUN <dawin00k@gmail.com>
2026-03-20 13:03:35 +0900

    day7: Sequences and interval queries 4

--- programmers/src/javaBasicCodingTraining/Day_7_반복문/_01_수열과_구간_쿼리_4/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_7_반복문/_01_수열과_구간_쿼리_4/Solution.java
@@ -1,18 +1,34 @@
-//
-//
+// arr : [0, 1, 2, 4, 3]
+// queries : [[0, 4, 1], [0, 3, 2], [0, 3, 3]]
+// result : [ 3, 2, 4, 6, 4]
 
 package javaBasicCodingTraining.Day_7_반복문._01_수열과_구간_쿼리_4;
 
+import java.util.Arrays;
+
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int[] solution(int[] arr, int[][] queries) {
+
+		for (int[] query : queries) {
+			int s = query[0];
+			int e = query[1];
+			int k = query[2];
+
+			for (int i = s; i <= e; i++) {
+				if (i % k == 0) {
+					arr[i] += 1;
+				}
+			}
+		}
+
+		return arr;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		int[] result = sol.solution(new int[] { 0, 1, 2, 4, 3 }, new int[][] { { 0, 4, 1 }, { 0, 3, 2 }, { 0, 3, 3 } });
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
