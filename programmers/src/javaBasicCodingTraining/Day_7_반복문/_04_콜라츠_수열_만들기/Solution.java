d8aa6996b947bb6780007f3fe509fb1705a2d8d1
HUN <dawin00k@gmail.com>
2026-03-20 14:53:19 +0900

    day7: Creating the Collatz Sequence

--- programmers/src/javaBasicCodingTraining/Day_7_반복문/_04_콜라츠_수열_만들기/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_7_반복문/_04_콜라츠_수열_만들기/Solution.java
@@ -1,18 +1,40 @@
-//
-//
+//	n : 10
+//	result : [10, 5, 16, 8, 4, 2, 1]
 
 package javaBasicCodingTraining.Day_7_반복문._04_콜라츠_수열_만들기;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+// 다른 사람 풀이
+import java.util.stream.IntStream;
+
 public class Solution {
-	public int solution(int a, int b) {
-		return a + b;
+	public int[] solution(int n) {
+		List<Integer> list = new ArrayList<>();
+
+		list.add(n);
+
+		while (n != 1) {
+			n = n % 2 == 0 ? n / 2 : 3 * n + 1;
+			list.add(n);
+		}
+
+		return list.stream().mapToInt(i -> i).toArray();
+	}
+
+	// 다른 사람 풀이
+	public int[] solution2(int n) {
+		return IntStream.concat(IntStream.iterate(n, i -> i > 1, i -> i % 2 == 0 ? i / 2 : 3 * i + 1), IntStream.of(1))
+				.toArray();
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		int[] result = sol.solution2(10);
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
