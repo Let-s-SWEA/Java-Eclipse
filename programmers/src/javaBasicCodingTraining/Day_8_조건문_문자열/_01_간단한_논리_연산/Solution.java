5a9b6ae4fe3d29196936eed2e57ff87c5ba238d0
HUN <dawin00k@gmail.com>
2026-03-22 20:06:19 +0900

    day8: Simple Logic Calculation

--- programmers/src/javaBasicCodingTraining/Day_8_조건문_문자열/_01_간단한_논리_연산/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_8_조건문_문자열/_01_간단한_논리_연산/Solution.java
@@ -1,17 +1,24 @@
-//
-// 
+// (x1 ∨ x2) ∧ (x3 ∨ x4)
+// ∨ : OR / ∧ : AND
+// x1 : false
+// x2 : true
+// x3 : true
+// x4 : true
+// result : true
 
 package javaBasicCodingTraining.Day_8_조건문_문자열._01_간단한_논리_연산;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return 1;
+	public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+		
+		
+		return (x1 || x2) && (x3 || x4);
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		boolean result = sol.solution(false, true, true, true);
 
 		System.out.println(result);
 	}
