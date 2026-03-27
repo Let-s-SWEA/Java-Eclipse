1ccee02f7227e4eb4e84dbbf254564360fec6b37
HUN <dawin00k@gmail.com>
2026-03-27 18:33:00 +0900

    day9: n words after the string

--- programmers/src/javaBasicCodingTraining/Day_9_문자열/_03_문자열의_뒤의_n글자/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_9_문자열/_03_문자열의_뒤의_n글자/Solution.java
@@ -1,18 +1,21 @@
-// 
-// 
+// my_string : "ProgrammerS123"
+// n : 11
+// result : "grammerS123"
+// str.substring(a, b) : str의 a번째부터 b번째까지
+// str.substring(a) : str의 a번째부터 끝까지
 
 package javaBasicCodingTraining.Day_9_문자열._03_문자열의_뒤의_n글자;
 
-public class Solution{
-	public int solution(int a, int b) {
-		return 1;
+public class Solution {
+	public String solution(String my_string, int n) {
+		return my_string.substring(my_string.length() - n);
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		String result = sol.solution("He110W0r1d", 5);
+
 		System.out.println(result);
 	}
 }
