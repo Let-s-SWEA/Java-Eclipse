4607f751268b17960bd0d4fe52b1f07f102a9a19
HUN <dawin00k@gmail.com>
2026-03-28 17:57:07 +0900

    day10: QR Code

--- programmers/src/javaBasicCodingTraining/Day_10_문자열/_05_qr_code/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_10_문자열/_05_qr_code/Solution.java
@@ -1,18 +1,26 @@
-// 
-// 
+// q : 3
+// r : 1
+// code : "qjnwezgrpirldywt"
+// result : "jerry"
 
 package javaBasicCodingTraining.Day_10_문자열._05_qr_code;
 
-public class Solution{
-	public int solution(int a, int b) {
-		return 1;
+public class Solution {
+	public String solution(int q, int r, String code) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = r; i < code.length(); i += q) {
+			sb.append(code.charAt(i));
+		}
+
+		return sb.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		String result = sol.solution(3, 1, "qjnwezgrpirldywt");
+
 		System.out.println(result);
 	}
 }
