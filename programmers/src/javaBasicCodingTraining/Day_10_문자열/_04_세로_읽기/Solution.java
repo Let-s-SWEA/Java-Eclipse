f75e14ffeb68df0f4655bcab783556c0527f9174
HUN <dawin00k@gmail.com>
2026-03-28 17:50:54 +0900

    day10: Vertical Reading

--- programmers/src/javaBasicCodingTraining/Day_10_문자열/_04_세로_읽기/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_10_문자열/_04_세로_읽기/Solution.java
@@ -1,18 +1,26 @@
-// 
-// 
+// my_string : "ihrhbakrfpndopljhygc"
+// m : 4
+// c : 2
+// result : "happy"
 
 package javaBasicCodingTraining.Day_10_문자열._04_세로_읽기;
 
-public class Solution{
-	public int solution(int a, int b) {
-		return 1;
+public class Solution {
+	public String solution(String my_string, int m, int c) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = c - 1; i < my_string.length(); i += m) {
+			sb.append(my_string.charAt(i));
+		}
+
+		return sb.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+
+		String result = sol.solution("ihrhbakrfpndopljhygc", 4, 2);
+
 		System.out.println(result);
 	}
 }
