feb33981fadedc8d24cd33240bc5f155cc747586
HUN <dawin00k@gmail.com>
2026-03-28 16:45:46 +0900

    day10: n words at the beginning of the array

--- programmers/src/javaBasicCodingTraining/Day_10_문자열/_01_문자열의_앞의_n글자/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_10_문자열/_01_문자열의_앞의_n글자/Solution.java
@@ -1,17 +1,22 @@
-// 
-// 
+// my_string : "ProgrammerS123"
+// n : 11
+// result : "ProgammerS"
+
+// my_string : "He110W0r1d"
+// n : 5
+// result : "He110"
 
 package javaBasicCodingTraining.Day_10_문자열._01_문자열의_앞의_n글자;
 
 public class Solution{
-	public int solution(int a, int b) {
-		return 1;
+	public String solution(String my_string, int n) {
+		return my_string.substring(0, n);
 	}
 	
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		
-		int result = sol.solution(0, 0);
+		String result = sol.solution("ProgrammerS123", 11);
 		
 		System.out.println(result);
 	}
