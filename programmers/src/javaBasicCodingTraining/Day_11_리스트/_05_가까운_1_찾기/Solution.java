35a58c3fd8bd8d1dd891dbd0c02f772c2ef79663
HUN <dawin00k@gmail.com>
2026-04-01 22:54:38 +0900

    day11: Find the nearest 1

--- programmers/src/javaBasicCodingTraining/Day_11_리스트/_05_가까운_1_찾기/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_11_리스트/_05_가까운_1_찾기/Solution.java
@@ -1,18 +1,25 @@
-// 
-// 
+// arr : [0, 0, 0, 1]
+// idx : 1
+// result : 3
 
 package javaBasicCodingTraining.Day_11_리스트._05_가까운_1_찾기;
 
-public class Solution{
-	public int solution(int a, int b) {
-		return 1;
+public class Solution {
+	public int solution(int[] arr, int idx) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 1 && i >= idx) {
+				return i;
+			}
+		}
+
+		return -1;
 	}
-	
+
 	public static void main(String[] args) {
-		Solution sol  = new Solution();
-		
-		int result = sol.solution(0, 0);
-		
+		Solution sol = new Solution();
+
+		int result = sol.solution(new int[] { 1, 1, 1, 1, 0 }, 3);
+
 		System.out.println(result);
 	}
 }
