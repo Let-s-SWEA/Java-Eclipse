06ac592973ec3a94752345e34c4a7620f11b66b5
HUN <dawin00k@gmail.com>
2026-04-03 15:34:30 +0900

    day12: The first negative number

--- programmers/src/javaBasicCodingTraining/Day_12_리스트/_02_첫_번째로_나오는_음수/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_12_리스트/_02_첫_번째로_나오는_음수/Solution.java
@@ -1,17 +1,23 @@
-// 
-// 
+// num_list : [2, 4, 15, 46, 38, -2, 15]
+// result : 5
 
 package javaBasicCodingTraining.Day_12_리스트._02_첫_번째로_나오는_음수;
 
 public class Solution {
-	public int solution(int a, int b) {
-		return 1;
+	public int solution(int[] num_list) {
+		for(int idx = 0; idx < num_list.length; idx++) {
+			if(num_list[idx] < 0) {
+				return idx;
+			}
+		}
+		
+		return -1;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		int result = sol.solution(new int[] { 2, 4, 15, 46, 38, -2, 15 });
 
 		System.out.println(result);
 	}
