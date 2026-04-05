85861b92d0597b221754af5b6c8ea797c78c1dfb
HUN <dawin00k@gmail.com>
2026-04-05 17:18:55 +0900

    day13: Up to the nth element

--- programmers/src/javaBasicCodingTraining/Day_13_리스트/_04_n_번째_원소까지/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_13_리스트/_04_n_번째_원소까지/Solution.java
@@ -1,18 +1,24 @@
-// 
-// 
+// num_list : [2, 1, 6]
+// n : 1
+// result : [2]
 
 package javaBasicCodingTraining.Day_13_리스트._04_n_번째_원소까지;
 
+import java.util.Arrays;
+
 public class Solution {
-	public int solution(int a, int b) {
-		return 1;
+	public int[] solution(int[] num_list, int n) {
+		int[] answer = new int[n];
+		System.arraycopy(num_list, 0, answer, 0, n);
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		int[] result = sol.solution(new int[] { 5, 2, 1, 7, 5 }, 3);
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
