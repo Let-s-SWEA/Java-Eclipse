102fa55b82939cf76b0346dedcc9ac80f0a57575
HUN <dawin00k@gmail.com>
2026-04-05 17:12:50 +0900

    day13: Left Right

--- programmers/src/javaBasicCodingTraining/Day_13_리스트/_03_왼쪽_오른쪽/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_13_리스트/_03_왼쪽_오른쪽/Solution.java
@@ -1,18 +1,27 @@
-// 
+// str_list : ["u", "u", "l", "r"]
 // 
 
 package javaBasicCodingTraining.Day_13_리스트._03_왼쪽_오른쪽;
 
+import java.util.Arrays;
+
 public class Solution {
-	public int solution(int a, int b) {
-		return 1;
+	public String[] solution(String[] str_list) {
+		for (int i = 0; i < str_list.length; i++) {
+			if (str_list[i] == "l") {
+				return Arrays.copyOfRange(str_list, 0, i);
+			} else if (str_list[i] == "r") {
+				return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+			}
+		}
+		return new String[0];
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0, 0);
+		String[] result = sol.solution(new String[] { "u", "u", "l", "r" });
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
