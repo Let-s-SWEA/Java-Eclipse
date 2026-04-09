9ac7d570bea7dd57b0120ae79925db30cdd01ad3
HUN <dawin00k@gmail.com>
2026-04-10 00:25:39 +0900

    bj: Number of Verification

--- BaekJoon/src/p2475_검증수/Main.java
+++ BaekJoon/src/p2475_검증수/Main.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 
-class Solution{
+class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		System.setIn(new FileInputStream("src/p2475_검증수/input.txt"));
-		
+
 		Scanner sc = new Scanner(System.in);
+
+		int temp = 0;
+
+		for (int i = 0; i < 5; i++) {
+			int num = sc.nextInt();
+
+			temp += (num * num);
+		}
+
+		System.out.print(temp % 10);
+		sc.close();
 	}
 }
