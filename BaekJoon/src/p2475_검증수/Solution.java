5d23f348c240133795a9ed382e54a4bca16f1462
HUN <dawin00k@gmail.com>
2026-04-10 00:12:49 +0900

    bj: Setting2

---
+++ BaekJoon/src/p2475_검증수/Solution.java
@@ -0,0 +1,14 @@
+
+package p2475_검증수;
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+
+class Solution{
+	public static void main(String[] args) throws FileNotFoundException {
+		System.setIn(new FileInputStream("src/p2475_검증수/input.txt"));
+		
+		Scanner sc = new Scanner(System.in);
+	}
+}
