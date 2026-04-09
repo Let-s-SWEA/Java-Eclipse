18b31ee486af5bbf890a6a8ad33d442596468c43
HUN <dawin00k@gmail.com>
2026-04-10 00:27:05 +0900

    bj: Not Saved Contents

--- BaekJoon/src/p2475_검증수/Main.java
+++ BaekJoon/src/p2475_검증수/Main.java
@@ -2,13 +2,34 @@
 package p2475_검증수;
 
 import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileInputStream;
+//
+//class Main {
+//	public static void main(String[] args) throws FileNotFoundException {
+//		System.setIn(new FileInputStream("src/p2475_검증수/input.txt"));
+//
+//		Scanner sc = new Scanner(System.in);
+//
+//		int temp = 0;
+//
+//		for (int i = 0; i < 5; i++) {
+//			int num = sc.nextInt();
+//
+//			temp += (num * num);
+//		}
+//
+//		System.out.print(temp % 10);
+//		sc.close();
+//	}
+//}
 
-class Main {
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("src/p2475_검증수/input.txt"));
+// 제출용
 
+import java.util.Scanner;
+
+class Main{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int temp = 0;
