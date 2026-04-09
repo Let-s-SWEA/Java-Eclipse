cb610bd08901f41c810f1f0c0ed22d8194128e0e
HUN <dawin00k@gmail.com>
2026-04-09 13:24:57 +0900

    day14: To Do List

--- programmers/src/javaBasicCodingTraining/Day_14_리스트/_03_할_일_목록/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_14_리스트/_03_할_일_목록/Solution.java
@@ -3,16 +3,37 @@
 
 package javaBasicCodingTraining.Day_14_리스트._03_할_일_목록;
 
+import java.util.Arrays;
+
 class Solution {
-	public int solution(int a) {
-		return 1;
+	public String[] solution(String[] todo_list, boolean[] finished) {
+		int count = 0;
+
+		for (int i = 0; i < finished.length; i++) {
+			if (!finished[i]) {
+				count++;
+			}
+		}
+
+		String[] answer = new String[count];
+
+		for (int j = 0, idx = 0; j < todo_list.length; j++) {
+			if (!finished[j]) {
+				answer[idx++] = todo_list[j];
+			}
+		}
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0);
+		String[] result = sol.solution(
+				new String[] { "problemsolving", "practiceguitar", "swim", "studygraph" },
+				new boolean[] { true, false, true, false }
+		);
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
