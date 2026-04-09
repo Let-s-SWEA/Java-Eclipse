6f67b01997b845d5043ed458b38675823030c420
HUN <dawin00k@gmail.com>
2026-04-09 14:10:46 +0900

    day14: Sequences and Intervals Curriculumn 1

--- programmers/src/javaBasicCodingTraining/Day_14_리스트/_05_수열과_구간_쿼리_1/Solution.java
+++ programmers/src/javaBasicCodingTraining/Day_14_리스트/_05_수열과_구간_쿼리_1/Solution.java
@@ -1,18 +1,23 @@
-// 
-// 
-
 package javaBasicCodingTraining.Day_14_리스트._05_수열과_구간_쿼리_1;
 
+import java.util.Arrays;
+
 class Solution {
-	public int solution(int a) {
-		return 1;
+	public int[] solution(int[] arr, int[][] queries) {
+		for (int[] query : queries) {
+			for (int i = query[0]; i <= query[1]; i++) {
+				arr[i]++;
+			}
+		}
+
+		return arr;
 	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 
-		int result = sol.solution(0);
+		int[] result = sol.solution(new int[] { 0, 1, 2, 3, 4 }, new int[][] { { 0, 1 }, { 1, 2 }, { 2, 3 } });
 
-		System.out.println(result);
+		System.out.println(Arrays.toString(result));
 	}
 }
